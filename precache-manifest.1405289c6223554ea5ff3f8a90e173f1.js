8e5eca966cf473b454e737f51e3affc4f453b6e3
elopezt <00014718@uca.edu.sv>
2019-11-01 11:28:49 -0600

    Updates

---
+++ precache-manifest.1405289c6223554ea5ff3f8a90e173f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e813511242ac8cfaf45e27433a435482",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "4b5aa2d7a14f0edab6ac",
+    "url": "/tictactoe/static/css/main.37f8b982.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "4b5aa2d7a14f0edab6ac",
+    "url": "/tictactoe/static/js/main.10e8eb55.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
